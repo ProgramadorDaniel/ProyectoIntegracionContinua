d5d371774cc9933f89b34c68c183e1fc74e10c71
ProgramadorDaniel <74018316+ProgramadorDaniel@users.noreply.github.com>
2021-05-21 11:07:37 -0500

    Add files via upload

---
+++ scripts/gallery.js
@@ -0,0 +1,298 @@
+﻿
+$defaultViewMode="full"; //// completa y normal , original
+$tsMargin=30; //primero y último margen de miniaturas ( para una mejor interacción cursor)
+$scrollEasing=600; //desplácese hacia la flexibilización cantidad ( 0 para no flexibilización )
+$scrollEasingType="easeOutCirc"; //Tipo de flexibilización de desplazamiento
+$thumbnailsContainerOpacity=0.8; //área de miniaturas opacidad por defecto
+$thumbnailsContainerMouseOutOpacity=0; //miniaturas área de opacidad en ratón fuera
+$thumbnailsOpacity=0.6; //opacidad miniaturas por defecto
+$nextPrevBtnsInitState="show"; //siguientes botones de imagen anteriores / estado inicial ( "ocultar " o "show" )
+$keyboardNavigation="on"; //permitir la navegación por teclado / desactivar ( "on" u "off" )
+
+//cache vars
+$thumbnails_wrapper=$("#thumbnails_wrapper");
+$outer_container=$("#outer_container");
+$thumbScroller=$(".thumbScroller");
+$thumbScroller_container=$(".thumbScroller .container");
+$thumbScroller_content=$(".thumbScroller .content");
+$thumbScroller_thumb=$(".thumbScroller .thumb");
+$preloader=$("#preloader");
+$toolbar=$("#toolbar");
+$toolbar_a=$("#toolbar a");
+$bgimg=$("#bgimg");
+$img_title=$("#img_title");
+$nextImageBtn=$(".nextImageBtn");
+$prevImageBtn=$(".prevImageBtn");
+
+$(window).load(function() {
+	$toolbar.data("imageViewMode",$defaultViewMode); //modo de vista por defecto
+	if($defaultViewMode=="full"){
+		$toolbar_a.html("<img src='images/toolbar_n_icon.png' width='50' height='50'  />").attr("onClick", "ImageViewMode('normal');return false").attr("title", "Restore");
+	} else {
+		$toolbar_a.html("<img src='images/toolbar_fs_icon.png' width='50' height='50'  />").attr("onClick", "ImageViewMode('full');return false").attr("title", "Maximize");
+	}
+	ShowHideNextPrev($nextPrevBtnsInitState);
+	//scroller miniatura
+	$thumbScroller_container.css("marginLeft",$tsMargin+"px"); //añadir margen
+	sliderLeft=$thumbScroller_container.position().left;
+	sliderWidth=$outer_container.width();
+	$thumbScroller.css("width",sliderWidth);
+	var totalContent=0;
+	fadeSpeed=200;
+	
+	var $the_outer_container=document.getElementById("outer_container");
+	var $placement=findPos($the_outer_container);
+	
+	$thumbScroller_content.each(function () {
+		var $this=$(this);
+		totalContent+=$this.innerWidth();
+		$thumbScroller_container.css("width",totalContent);
+		$this.children().children().children(".thumb").fadeTo(fadeSpeed, $thumbnailsOpacity);
+	});
+
+	$thumbScroller.mousemove(function(e){
+		if($thumbScroller_container.width()>sliderWidth){
+	  		var mouseCoords=(e.pageX - $placement[1]);
+	  		var mousePercentX=mouseCoords/sliderWidth;
+	  		var destX=-((((totalContent+($tsMargin*2))-(sliderWidth))-sliderWidth)*(mousePercentX));
+	  		var thePosA=mouseCoords-destX;
+	  		var thePosB=destX-mouseCoords;
+	  		if(mouseCoords>destX){
+		  		$thumbScroller_container.stop().animate({left: -thePosA}, $scrollEasing,$scrollEasingType); 
+	  		} else if(mouseCoords<destX){
+		  		$thumbScroller_container.stop().animate({left: thePosB}, $scrollEasing,$scrollEasingType); 
+				$thumbScroller_container.stop();  
+	  		}
+		}
+	});
+
+	$thumbnails_wrapper.fadeTo(fadeSpeed, $thumbnailsContainerOpacity);
+	$thumbnails_wrapper.hover(
+		function(){ //mouse over
+			var $this=$(this);
+			$this.stop().fadeTo("slow", 1);
+		},
+		function(){ //mouse out
+			var $this=$(this);
+			$this.stop().fadeTo("slow", $thumbnailsContainerMouseOutOpacity);
+		}
+	);
+
+	$thumbScroller_thumb.hover(
+		function(){ //mouse over
+			var $this=$(this);
+			$this.stop().fadeTo(fadeSpeed, 1);
+		},
+		function(){ //mouse out
+			var $this=$(this);
+			$this.stop().fadeTo(fadeSpeed, $thumbnailsOpacity);
+		}
+	);
+
+	//en la ventana de cambiar el tamaño de la imagen escala y restablecer scroller miniatura
+	$(window).resize(function() {
+		FullScreenBackground("#bgimg",$bgimg.data("newImageW"),$bgimg.data("newImageH"));
+		$thumbScroller_container.stop().animate({left: sliderLeft}, 400,"easeOutCirc"); 
+		var newWidth=$outer_container.width();
+		$thumbScroller.css("width",newWidth);
+		sliderWidth=newWidth;
+		$placement=findPos($the_outer_container);
+	});
+
+	//primera carga de imágenes
+	var the1stImg = new Image();
+	the1stImg.onload = CreateDelegate(the1stImg, theNewImg_onload);
+	the1stImg.src = $bgimg.attr("src");
+	$outer_container.data("nextImage",$(".content").first().next().find("a").attr("href"));
+	$outer_container.data("prevImage",$(".content").last().find("a").attr("href"));
+});
+
+function BackgroundLoad($this,imageWidth,imageHeight,imgSrc){
+	$this.fadeOut("fast",function(){
+		$this.attr("src", "").attr("src", imgSrc); //cambio de fuente de imagen
+		FullScreenBackground($this,imageWidth,imageHeight); //imagen de fondo de escala
+		$preloader.fadeOut("fast",function(){$this.fadeIn("slow");});
+		var imageTitle=$img_title.data("imageTitle");
+		if(imageTitle){
+			$this.attr("alt", imageTitle).attr("title", imageTitle);
+			$img_title.fadeOut("fast",function(){
+				$img_title.html(imageTitle).fadeIn();
+			});
+		} else {
+			$img_title.fadeOut("fast",function(){
+				$img_title.html($this.attr("title")).fadeIn();
+			});
+		}
+	});
+}
+
+//barra de herramientas mouseover
+if($toolbar.css("display")!="none"){
+	$toolbar.fadeTo("fast", 0.4);
+}
+$toolbar.hover(
+	function(){ //mouse over
+		var $this=$(this);
+		$this.stop().fadeTo("fast", 1);
+	},
+	function(){ //mouse out
+		var $this=$(this);
+		$this.stop().fadeTo("fast", 0.4);
+	}
+);
+
+//Al hacer clic en la imagen en miniatura cambia la imagen de fondo
+$("#outer_container a").click(function(event){
+	event.preventDefault();
+	var $this=$(this);
+	GetNextPrevImages($this);
+	GetImageTitle($this);
+	SwitchImage(this);
+	ShowHideNextPrev("show");
+}); 
+
+//Imágenes siguiente / ant botones
+$nextImageBtn.click(function(event){
+	event.preventDefault();
+	SwitchImage($outer_container.data("nextImage"));
+	var $this=$("#outer_container a[href='"+$outer_container.data("nextImage")+"']");
+	GetNextPrevImages($this);
+	GetImageTitle($this);
+});
+
+$prevImageBtn.click(function(event){
+	event.preventDefault();
+	SwitchImage($outer_container.data("prevImage"));
+	var $this=$("#outer_container a[href='"+$outer_container.data("prevImage")+"']");
+	GetNextPrevImages($this);
+	GetImageTitle($this);
+});
+
+//próximos imágenes / prev flechas del teclado
+if($keyboardNavigation=="on"){
+$(document).keydown(function(ev) {
+    if(ev.keyCode == 39) { //flecha derecha
+        SwitchImage($outer_container.data("nextImage"));
+		var $this=$("#outer_container a[href='"+$outer_container.data("nextImage")+"']");
+		GetNextPrevImages($this);
+		GetImageTitle($this);
+        return false; // no ejecutar la acción predeterminada ( desplazamiento o lo que sea )
+    } else if(ev.keyCode == 37) { //left arrow
+        SwitchImage($outer_container.data("prevImage"));
+		var $this=$("#outer_container a[href='"+$outer_container.data("prevImage")+"']");
+		GetNextPrevImages($this);
+		GetImageTitle($this);
+        return false; // no ejecutar la acción predeterminada ( desplazamiento o lo que sea )
+    }
+});
+}
+
+function ShowHideNextPrev(state){
+	if(state=="hide"){
+		$nextImageBtn.fadeOut();
+		$prevImageBtn.fadeOut();
+	} else {
+		$nextImageBtn.fadeIn();
+		$prevImageBtn.fadeIn();
+	}
+}
+
+//obtener título de la imagen
+function GetImageTitle(elem){
+	var title_attr=elem.children("img").attr("title"); //obtener el atributo título de la imagen
+	$img_title.data("imageTitle", title_attr); //almacén de imágenes título
+}
+
+//get next/prev images
+function GetNextPrevImages(curr){
+	var nextImage=curr.parents(".content").next().find("a").attr("href");
+	if(nextImage==null){ //si la última imagen , el próximo es primero
+		var nextImage=$(".content").first().find("a").attr("href");
+	}
+	$outer_container.data("nextImage",nextImage);
+	var prevImage=curr.parents(".content").prev().find("a").attr("href");
+	if(prevImage==null){ //si la primera imagen , anterior es pasado
+		var prevImage=$(".content").last().find("a").attr("href");
+	}
+	$outer_container.data("prevImage",prevImage);
+}
+
+//imagen interruptor
+function SwitchImage(img){
+	$preloader.fadeIn("fast"); 
+	var theNewImg = new Image();
+	theNewImg.onload = CreateDelegate(theNewImg, theNewImg_onload);
+	theNewImg.src = img;
+}
+
+//obtener nuevas dimensiones de imagen
+function CreateDelegate(contextObject, delegateMethod){
+	return function(){
+		return delegateMethod.apply(contextObject, arguments);
+	}
+}
+
+//nueva imagen de la carga
+function theNewImg_onload(){
+	$bgimg.data("newImageW",this.width).data("newImageH",this.height);
+	BackgroundLoad($bgimg,this.width,this.height,this.src);
+}
+
+//Funcion escala de imagen
+function FullScreenBackground(theItem,imageWidth,imageHeight){
+	var winWidth=$(window).width();
+	var winHeight=$(window).height();
+	if($toolbar.data("imageViewMode")!="original"){ //escala
+		var picHeight = imageHeight / imageWidth;
+		var picWidth = imageWidth / imageHeight;
+		if($toolbar.data("imageViewMode")=="full"){ //modo de tamaño de imagen completa
+			if ((winHeight / winWidth) < picHeight) {
+				$(theItem).attr("width",winWidth);
+				$(theItem).attr("height",picHeight*winWidth);
+			} else {
+				$(theItem).attr("height",winHeight);
+				$(theItem).attr("width",picWidth*winHeight);
+			};
+		} else { //modo normal tamaño imagen 
+			if ((winHeight / winWidth) > picHeight) {
+				$(theItem).attr("width",winWidth);
+				$(theItem).attr("height",picHeight*winWidth);
+			} else {
+				$(theItem).attr("height",winHeight);
+				$(theItem).attr("width",picWidth*winHeight);
+			};
+		}
+		$(theItem).css("margin-left",(winWidth-$(theItem).width())/2);
+		$(theItem).css("margin-top",(winHeight-$(theItem).height())/2);
+	} else { //sin escala
+		$(theItem).attr("width",imageWidth);
+		$(theItem).attr("height",imageHeight);
+		$(theItem).css("margin-left",(winWidth-imageWidth)/2);
+		$(theItem).css("margin-top",(winHeight-imageHeight)/2);
+	}
+}
+
+//Imagen función de modo de visualización - Tamaño de pantalla completa o normal
+function ImageViewMode(theMode){
+	$toolbar.data("imageViewMode", theMode);
+	FullScreenBackground($bgimg,$bgimg.data("newImageW"),$bgimg.data("newImageH"));
+	if(theMode=="full"){
+		$toolbar_a.html("<img src='images/toolbar_n_icon.png' width='50' height='50'  />").attr("onClick", "ImageViewMode('normal');return false").attr("title", "Restore");
+	} else {
+		$toolbar_a.html("<img src='images/toolbar_fs_icon.png' width='50' height='50'  />").attr("onClick", "ImageViewMode('full');return false").attr("title", "Maximize");
+	}
+}
+
+//funcion para encontrar el elemento Posición
+	function findPos(obj) {
+		var curleft = curtop = 0;
+		if (obj.offsetParent) {
+			curleft = obj.offsetLeft
+			curtop = obj.offsetTop
+			while (obj = obj.offsetParent) {
+				curleft += obj.offsetLeft
+				curtop += obj.offsetTop
+			}
+		}
+		return [curtop, curleft];
+	}
---
+++ scripts/jquery.easing.1.3.js
@@ -0,0 +1,196 @@
+/*
+  JQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+ * Utiliza la construyeron en el alivio de las capacidades añadidas En jQuery 1.1
+ * Para ofrecer múltiples opciones de flexibilización
+ *
+ * CONDICIONES DE USO - jQuery Easing
+ *
+ * El código abierto bajo la licencia BSD.
+ *
+ * Copyright © 2008 George McGinley Smith
+ * Reservados todos los derechos.
+ *
+ * La redistribución y el uso en formas fuente y binario, con o sin modificación,
+ * Están permitidos siempre que se cumplan las siguientes condiciones:
+ *
+ * Las redistribuciones del código fuente deben conservar el aviso de copyright anterior, esta lista de
+ * Las condiciones y el siguiente descargo de responsabilidad.
+ * Las redistribuciones en formato binario deben reproducir el aviso de copyright anterior, esta lista
+ * De condiciones y el siguiente descargo de responsabilidad en la documentación y / u otros materiales
+ * Proporcionado con la distribución.
+ *
+ * Ni el nombre del autor ni los nombres de los colaboradores se puede utilizar para respaldar
+ * O promocionar productos derivados de este software sin el permiso previo por escrito.
+ *
+ *
+*/
+
+// t: tiempo actual , b : Principio de valor, c: cambio en el valor , d: la duración
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alerta ( jQuery.easing.default );
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+	easeInQuad: function (x, t, b, c, d) {
+		return c*(t/=d)*t + b;
+	},
+	easeOutQuad: function (x, t, b, c, d) {
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeInOutQuad: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t + b;
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInCubic: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t + b;
+	},
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	},
+	easeInOutCubic: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	},
+	easeInQuart: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutQuart: function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	easeInQuint: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	},
+	easeOutQuint: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	},
+	easeInOutQuint: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	},
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	},
+	easeInOutSine: function (x, t, b, c, d) {
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	},
+	easeInExpo: function (x, t, b, c, d) {
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	},
+	easeOutExpo: function (x, t, b, c, d) {
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	},
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	},
+	easeInCirc: function (x, t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	},
+	easeOutCirc: function (x, t, b, c, d) {
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	},
+	easeInOutCirc: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	},
+	easeInElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	easeOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},
+	easeInOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	},
+	easeInBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	easeOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	},
+	easeInOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158; 
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	easeInBounce: function (x, t, b, c, d) {
+		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+	},
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	},
+	easeInOutBounce: function (x, t, b, c, d) {
+		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+});
+
+/*
+ *
+* CONDICIONES DE USO - ECUACIONES flexibilización
+ *
+ * El código abierto bajo la licencia BSD.
+ *
+ * Copyright © 2001 Robert Penner
+ * Reservados todos los derechos.
+ *
+ * La redistribución y el uso en formas fuente y binario, con o sin modificación,
+ * Están permitidos siempre que se cumplan las siguientes condiciones:
+ *
+ * Las redistribuciones del código fuente deben conservar el aviso de copyright anterior, esta lista de
+ * Las condiciones y el siguiente descargo de responsabilidad.
+ * Las redistribuciones en formato binario deben reproducir el aviso de copyright anterior, esta lista
+ * De condiciones y el siguiente descargo de responsabilidad en la documentación y / u otros materiales
+ * Proporcionado con la distribución.
+ *
+ * Ni el nombre del autor ni los nombres de los colaboradores se puede utilizar para respaldar
+ * O promocionar productos derivados de este software sin el permiso previo por escrito.
+ *
+ * ESTE SOFTWARE SE PROPORCIONA POR LOS TITULARES DE DERECHOS DE AUTOR Y COLABORADORES "TAL CUAL" Y CUALQUIER
+ * EXPRESA O IMPLÍCITAS, INCLUYENDO, SIN LIMITACIÓN, LAS GARANTÍAS DE
+ * COMERCIALIZACIÓN Y APTITUD PARA UN PROPÓSITO PARTICULAR. EN NINGÚN CASO EL
+ * PROPIETARIO O LOS COLABORADORES DE AUTOR SERÁN RESPONSABLES POR DAÑOS DIRECTOS, INDIRECTOS, ACCIDENTALES, ESPECIALES,
+ * EJEMPLARES O INDIRECTOS (INCLUYENDO, PERO NO LIMITADO A, LA OBTENCIÓN DE SUSTITUCIÓN
+ * Bienes o servicios; Pérdida de uso, datos o beneficios; O INTERRUPCIÓN COMERCIAL) CAUSADOS
+ * Y EN CUALQUIER TEORÍA DE RESPONSABILIDAD, YA SEA EN CONTRATO, RESPONSABILIDAD ESTRICTA O AGRAVIO (INCLUYENDO
+ * NEGLIGENCIA O DE OTRA MANERA) POR DAÑOS OCASIONADOS POR EL USO DE ESTE SOFTWARE, INCLUSO SI SE HA ADVERTIDO
+ * DE LA POSIBILIDAD DE TALES DAÑOS.
+ *
+ */
